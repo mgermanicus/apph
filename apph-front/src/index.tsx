4bb2ba7b8ffea7e7423da04f94986b34a4b9a11e
ElieRavasse <elie.ravasse@viseo.com>
2022-04-13 17:41:02 +0200

    [feature][APPH-33] create login component and add mui dependencies

--- apph-front/src/index.tsx
+++ apph-front/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './pages/Login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ apph-front/src/pages/Login.tsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <header>Test</header>;
+}
+
+export default Login;
