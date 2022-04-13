35f655f145703635ee73fbdaaeb4c03a4e2212f3
Wassim Bouhtout <wassim.bouhtout@viseo.com>
2022-04-13 17:06:08 +0200

    refacto

--- apph-back/src/main/java/com/viseo/apph/ApphBackApplication.java
+++ apph-back/src/main/java/com/viseo/apph/ApphBackApplication.java
@@ -1,4 +1,4 @@
-package com.example.apphback;
+package com.viseo.apph;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- apph-back/src/main/java/com/viseo/apph/security/WebSecurityConfig.java
+++ apph-back/src/main/java/com/viseo/apph/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.apphback.security;
+package com.viseo.apph.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
