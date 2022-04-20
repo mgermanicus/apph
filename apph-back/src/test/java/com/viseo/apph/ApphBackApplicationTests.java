d6ecae2d5e1c53475a0ff3073f1ad00199569c1e
Yunan <yunan8@outlook.com>
2022-04-20 14:28:12 +0200

    [chore][apph-94] add main method test

--- apph-back/src/test/java/com/viseo/apph/ApphBackApplicationTests.java
+++ apph-back/src/test/java/com/viseo/apph/ApphBackApplicationTests.java
@@ -1,15 +1,19 @@
 package com.viseo.apph;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.UnsatisfiedDependencyException;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertThrows;
 
 // Test class added to cover main() invocation
 @SpringBootTest
 class ApphBackApplicationTests {
 
     @Test
-    void applicationContextTest() {
-        ApphBackApplication.main(new String[] {});
+    void contextShouldNotLoadWhenPropertiesIncorrect() {
+        assertThrows(UnsatisfiedDependencyException.class, () ->
+            ApphBackApplication.main(new String[] {"--spring.profiles.active=incorrect"}));
     }
 
 }
