e0b4a69f8468d48bb09e55613726782836fbd5c9
Yunan <yunan8@outlook.com>
2022-04-21 15:04:35 +0200

    [feature][APPH-4] fix exception during GitHub test

--- apph-back/src/test/java/com/viseo/apph/ApphBackApplicationTests.java
+++ apph-back/src/test/java/com/viseo/apph/ApphBackApplicationTests.java
@@ -1,13 +1,20 @@
 package com.viseo.apph;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.UnsatisfiedDependencyException;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.Assert.assertThrows;
+
+// Test class added to cover main() invocation
+@SpringBootTest(classes = ApphBackApplicationTests.class)
 class ApphBackApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextShouldNotLoadWhenPropertiesIncorrect() {
+        // Given
+        assertThrows(UnsatisfiedDependencyException.class, () ->
+            ApphBackApplication.main(new String[] {"--spring.profiles.active=incorrect"}));
     }
 
 }
