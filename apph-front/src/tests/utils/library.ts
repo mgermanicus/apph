8a6c98858d8b9ba0b6854c262504632afd5177a8
ElieRavasse <elie.ravasse@viseo.com>
2022-04-22 17:55:34 +0200

    [feature][APPH-33] add tests front

---
+++ apph-front/src/tests/utils/library.ts
@@ -0,0 +1,39 @@
+import { fireEvent, screen } from '@testing-library/react';
+import Server from '../../services/Server';
+
+export function fillText(label: RegExp, value: string) {
+  const textInput = screen.getByRole('textbox', { name: label });
+  fireEvent.change(textInput, { target: { value: value } });
+}
+
+export function fillPassword(label: RegExp, value: string) {
+  const passwordInput = screen.getByLabelText(label);
+  fireEvent.change(passwordInput, { target: { value: value } });
+}
+
+export function clickButton(label: RegExp) {
+  fireEvent.click(screen.getByRole('button', { name: label }));
+}
+
+export function triggerRequestSuccess(response: string) {
+  Server.request = function (
+    URL: string,
+    requestOptions: RequestInit,
+    successFunction: (body: string) => void | undefined
+  ) {
+    successFunction(response);
+    return Promise.resolve();
+  };
+}
+
+export function triggerRequestFailure(response: string) {
+  Server.request = function (
+    URL: string,
+    requestOptions: RequestInit,
+    successFunction: (body: string) => void | undefined,
+    errorFunction: (error: string) => void
+  ) {
+    errorFunction(response);
+    return Promise.resolve();
+  };
+}
