364bdc0da9e694b83c2875ed6849d1d3f470ef8b
Yunan <yunan8@outlook.com>
2022-04-25 12:03:43 +0200

    [feature][APPH-4] complete test for PhotoControllerTest

--- apph-back/src/test/java/com/viseo/apph/controller/PhotoControllerTest.java
+++ apph-back/src/test/java/com/viseo/apph/controller/PhotoControllerTest.java
@@ -1,5 +1,7 @@
 package com.viseo.apph.controller;
 
+import com.viseo.apph.domain.Photo;
+import com.viseo.apph.dto.ResponseDTO;
 import com.viseo.apph.exception.InvalidFileException;
 import com.viseo.apph.service.PhotoService;
 import com.viseo.apph.service.S3Service;
@@ -8,11 +10,13 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -31,10 +35,29 @@ public class PhotoControllerTest {
         // Given
         MockMultipartFile file = new MockMultipartFile("file", "orig", null, "bar".getBytes());
         String name = "Test@";
+        Photo photo = new Photo();
         // When
-        assertThrows(RuntimeException.class,
-                () -> photoController.upload(file,name));
+        when(photoService.addPhoto(name)).thenReturn(photo);
+        ResponseEntity<ResponseDTO> responseEntity = photoController.upload(file,name);
         // Then
         verify(photoService, times(1)).addPhoto(any());
+        verify(s3Service, times(1)).saveWithName(any(),anyString());
+        assertEquals(responseEntity.getStatusCode().toString()
+                , HttpStatus.OK.toString());
+    }
+
+    @Test
+    public void testUploadException() throws InvalidFileException, IOException {
+        // Given
+        MockMultipartFile file = new MockMultipartFile("file", "orig", null, "bar".getBytes());
+        String name = "Test@";
+        Photo photo = new Photo();
+        // When
+        when(photoService.addPhoto(name)).thenReturn(photo);
+        when(s3Service.saveWithName(any(),anyString())).thenThrow(InvalidFileException.class);
+        ResponseEntity<ResponseDTO> responseEntity = photoController.upload(file,name);
+        // Then
+        assertEquals(responseEntity.getStatusCode().toString()
+                , HttpStatus.INTERNAL_SERVER_ERROR.toString());
     }
 }
