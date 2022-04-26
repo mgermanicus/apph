fe3cd94882be3cd0771eef5586e569028bb17144
alexandre-hu <79249762+alexandre-hu@users.noreply.github.com>
2022-04-26 17:48:53 +0200

    [refator][APPH-30] run prettier

--- apph-front/src/privateRoutes.tsx
+++ apph-front/src/privateRoutes.tsx
@@ -1,19 +1,34 @@
 import { Route, Routes } from 'react-router-dom';
 import { UserProfileContainer } from './static/containers/UserProfileContainer';
 import { SignIn } from './static/components/SignIn';
-import {TODOContainer} from "./static/containers/TODOContainer";
+import { TODOContainer } from './static/containers/TODOContainer';
 
 export const PrivateRoutes = (): JSX.Element => {
   return (
-      <Routes>
-          <Route path="/login" element={<SignIn />} />
-          <Route path="/me" element={<UserProfileContainer />} />
-          <Route path="/pictures" element={<TODOContainer todo="Page: Mes Photos" />} />
-          <Route path="/folders" element={<TODOContainer todo="Page: Mes Dossiers" />} />
-          <Route path="/trips" element={<TODOContainer todo="Page: Mes Voyages" />} />
-          <Route path="/tags" element={<TODOContainer todo="Page: Mes Tags" />} />
-          <Route path="/treatments" element={<TODOContainer todo="Page: Mes Traitements" />} />
-          <Route path="/research" element={<TODOContainer todo="Page: Recherche Avancée" />} />
-      </Routes>
+    <Routes>
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/me" element={<UserProfileContainer />} />
+      <Route
+        path="/pictures"
+        element={<TODOContainer todo="Page: Mes Photos" />}
+      />
+      <Route
+        path="/folders"
+        element={<TODOContainer todo="Page: Mes Dossiers" />}
+      />
+      <Route
+        path="/trips"
+        element={<TODOContainer todo="Page: Mes Voyages" />}
+      />
+      <Route path="/tags" element={<TODOContainer todo="Page: Mes Tags" />} />
+      <Route
+        path="/treatments"
+        element={<TODOContainer todo="Page: Mes Traitements" />}
+      />
+      <Route
+        path="/research"
+        element={<TODOContainer todo="Page: Recherche Avancée" />}
+      />
+    </Routes>
   );
 };
