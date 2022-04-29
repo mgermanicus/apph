2d5b0c79caa0cd82b0cb74d3ae3f677a3d20cd26
baptiste-viseo <100144253+baptiste-viseo@users.noreply.github.com>
2022-04-29 12:18:06 +0200

    [feature][APPH-31] refacto : remove router in index and use privaterouter

--- apph-front/src/index.tsx
+++ apph-front/src/index.tsx
@@ -2,14 +2,6 @@ import { App } from './App';
 import React from 'react';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
-import {
-  Route,
-  Routes,
-  Navigate,
-  BrowserRouter as Router
-} from 'react-router-dom';
-import { SignIn } from './static/components/SignIn';
-import { SignUp } from './static/components/SignUp';
 
 const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
