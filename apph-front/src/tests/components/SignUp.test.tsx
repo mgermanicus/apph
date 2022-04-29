b1053e00b056201c2f21c35f0764933ff02dbecc
baptiste-viseo <100144253+baptiste-viseo@users.noreply.github.com>
2022-04-29 10:26:06 +0200

    [feature][APPH-31] add default route that redirect to /

--- apph-front/src/tests/components/SignUp.test.tsx
+++ apph-front/src/tests/components/SignUp.test.tsx
@@ -13,7 +13,7 @@ import { useNavigate } from 'react-router-dom';
 
 const mockedUsedNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
-  ...(jest.requireActual('react-router-dom') as any),
+  ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockedUsedNavigate
 }));
 
