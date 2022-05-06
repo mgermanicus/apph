48bfbafb63e55252fcdb4b360cee3715da50da16
baptiste-viseo <100144253+baptiste-viseo@users.noreply.github.com>
2022-05-06 15:10:21 +0200

    [refacto][APPH-10] remove useless imports

--- apph-back/src/main/java/com/viseo/apph/service/FolderService.java
+++ apph-back/src/main/java/com/viseo/apph/service/FolderService.java
@@ -1,6 +1,5 @@
 package com.viseo.apph.service;
 
-import com.viseo.apph.dao.FolderDAO;
 import com.viseo.apph.dao.UserDAO;
 import com.viseo.apph.domain.Folder;
 import com.viseo.apph.dao.FolderDAO;
