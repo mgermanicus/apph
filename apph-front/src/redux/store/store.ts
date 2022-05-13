7290abc7ff89d4c5d480ada114d16a682679621e
ElieRavasse <elie.ravasse@viseo.com>
2022-05-13 17:09:23 +0200

    [refactor][APPH-11] add tag list in redux

--- apph-front/src/redux/store/store.ts
+++ apph-front/src/redux/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import currentUserSlice from '../slices/userSlice';
 import selectedPhotosSlice from '../slices/photoSlice';
+import tagListSlice from '../slices/tagSlice';
 
 export default configureStore({
   reducer: {
     currentUser: currentUserSlice,
-    selectedPhotos: selectedPhotosSlice
+    selectedPhotos: selectedPhotosSlice,
+    tagList: tagListSlice
   }
 });
