22dd280b24805d65bf18cace4a846126a557df8c
ElieRavasse <elie.ravasse@viseo.com>
2022-05-31 10:59:05 +0200

    [feature][APPH-106] add size radio

--- apph-front/src/static/components/DisplayPhoto.tsx
+++ apph-front/src/static/components/DisplayPhoto.tsx
@@ -1,17 +1,43 @@
 import { ITable } from '../../utils';
-import { Alert, Grid } from '@mui/material';
+import {
+  Alert,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  Radio,
+  RadioGroup
+} from '@mui/material';
 import { useEffect, useState } from 'react';
 import PhotoService from '../../services/PhotoService';
 import * as React from 'react';
 import PhotoDetails from './PhotoDetails';
+
+const tinySize = {
+  gridContainerSpacing: { xs: 2, md: 3 },
+  gridContainerColumns: { xs: 8, sm: 12, md: 16 },
+  gridContainerPadding: 5,
+  gridItemXS: 1,
+  gridItemSM: 1.5,
+  gridItemMD: 2
+};
 
 const mediumSize = {
   gridContainerSpacing: { xs: 2, md: 3 },
   gridContainerColumns: { xs: 8, sm: 12, md: 16 },
   gridContainerPadding: 5,
   gridItemXS: 2,
-  gridItemSM: 4,
+  gridItemSM: 3,
   gridItemMD: 4
+};
+
+const bigSize = {
+  gridContainerSpacing: { xs: 2, md: 3 },
+  gridContainerColumns: { xs: 8, sm: 12, md: 16 },
+  gridContainerPadding: 5,
+  gridItemXS: 4,
+  gridItemSM: 6,
+  gridItemMD: 8
 };
 
 export const DisplayPhoto = ({
@@ -21,7 +47,7 @@ export const DisplayPhoto = ({
 }): JSX.Element => {
   const [photoList, setPhotoList] = useState<ITable[]>([]);
   const [errorMessage, setErrorMessage] = useState('');
-  const [selectedSize, setSelectedSize] = useState(mediumSize);
+  const [selectedSize, setSelectedSize] = useState(tinySize);
 
   const getPhotos = async () => {
     await PhotoService.getFolderPhotos(
@@ -39,6 +65,23 @@ export const DisplayPhoto = ({
     getPhotos();
   }, [selectedFolder]);
 
+  function handleChangeSize(event: React.ChangeEvent<HTMLInputElement>) {
+    switch (event.target.value) {
+      case 'tiny':
+        setSelectedSize(tinySize);
+        break;
+      case 'medium':
+        setSelectedSize(mediumSize);
+        break;
+      case 'big':
+        setSelectedSize(bigSize);
+        break;
+      default:
+        setErrorMessage('Erreur lors de la modification du format');
+        break;
+    }
+  }
+
   if (errorMessage) {
     return (
       <Alert sx={{ mb: 2 }} severity="error">
@@ -47,34 +90,56 @@ export const DisplayPhoto = ({
     );
   } else {
     return (
-      <Grid
-        container
-        spacing={selectedSize.gridContainerSpacing}
-        columns={selectedSize.gridContainerColumns}
-        padding={selectedSize.gridContainerPadding}
-      >
-        {photoList.map((photo) => (
-          <Grid
-            item
-            xs={selectedSize.gridItemXS}
-            sm={selectedSize.gridItemSM}
-            md={selectedSize.gridItemMD}
-            key={'key' + photo.id}
+      <>
+        <FormControl sx={{ m: 1 }}>
+          <FormLabel id="photo-size-group-label">
+            Format d'affichage :
+          </FormLabel>
+          <RadioGroup
+            aria-labelledby="photo-size-group-label"
+            defaultValue="tiny"
+            name="photo-size-group"
+            row
+            onChange={handleChangeSize}
           >
-            <PhotoDetails
-              photoSrc={photo.url}
-              title={photo.title}
-              description={photo.description}
-              creationDate={photo.creationDate}
-              shootingDate={photo.shootingDate}
-              size={photo.size}
-              tags={photo.tags}
-              format={photo.format}
-              clickType="card"
+            <FormControlLabel value="tiny" control={<Radio />} label="Petit" />
+            <FormControlLabel
+              value="medium"
+              control={<Radio />}
+              label="Moyen"
             />
-          </Grid>
-        ))}
-      </Grid>
+            <FormControlLabel value="big" control={<Radio />} label="Grand" />
+          </RadioGroup>
+        </FormControl>
+        <Grid
+          container
+          spacing={selectedSize.gridContainerSpacing}
+          columns={selectedSize.gridContainerColumns}
+          padding={selectedSize.gridContainerPadding}
+        >
+          {photoList.map((photo) => (
+            <Grid
+              item
+              xs={selectedSize.gridItemXS}
+              sm={selectedSize.gridItemSM}
+              md={selectedSize.gridItemMD}
+              key={'key' + photo.id}
+            >
+              <PhotoDetails
+                photoSrc={photo.url}
+                title={photo.title}
+                description={photo.description}
+                creationDate={photo.creationDate}
+                shootingDate={photo.shootingDate}
+                size={photo.size}
+                tags={photo.tags}
+                format={photo.format}
+                clickType="card"
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </>
     );
   }
 };
