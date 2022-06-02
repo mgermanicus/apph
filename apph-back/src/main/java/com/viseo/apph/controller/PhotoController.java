4adf7efdc2a20b111e7246c7cd465be1eca94bf9
iattach <yunan.hou@viseo.com>
2022-06-02 11:02:14 +0200

    Update apph-back/src/main/java/com/viseo/apph/controller/PhotoController.java
    Co-authored-by: alexandre-hu <79249762+alexandre-hu@users.noreply.github.com>

--- apph-back/src/main/java/com/viseo/apph/controller/PhotoController.java
+++ apph-back/src/main/java/com/viseo/apph/controller/PhotoController.java
@@ -134,7 +134,6 @@ public class PhotoController {
             User user = utils.getUser();
             return ResponseEntity.ok(photoService.downloadZip(user, photosRequest));
         } catch (S3Exception | IOException e) {
-            e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MessageResponse("Une erreur est survenue lors du téléchargement"));
         } catch (UnauthorizedException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new MessageResponse("L'utilisateur n'est pas autorisé à accéder à la ressource demandée"));
